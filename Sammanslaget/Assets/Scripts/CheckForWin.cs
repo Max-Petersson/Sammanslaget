44360de95633c842b7a231e79dfa7b9021af2e64
Risalord <max.hej777@gmail.com>
2023-08-24 15:10:02 +0200

    Win lose screen

--- Sammanslaget/Assets/Scripts/CheckForWin.cs
+++ Sammanslaget/Assets/Scripts/CheckForWin.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CheckForWin : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool redRight = false;
     public bool boldRight = false;
+    private bool doOnce = false;
+    public GameObject endScreen;
     public void EndGame()
     {
+        
+        if(doOnce == true)
+        {
+            return;
+        }
+        FindObjectOfType<CheckForWin>().GetComponent<Button>().interactable = false;
+
         if(redRight && boldRight)
         {
-            Debug.Log("you Win");
+            SetTheText("You gitted good");
         }
         else
         {
-            Debug.Log("You Loose");
-            //lose
+            SetTheText("You died!");
         }
+        Destroy(FindObjectOfType<PlayerMovement>().gameObject);
+        doOnce = true;
+    }
+
+    private void SetTheText(string message)
+    {
+        endScreen.SetActive(true);
+        endScreen.GetComponentInChildren<TextMeshProUGUI>().text = message;
     }
 }
